d0717126768a9c2f12bae6cd8b9bada34ddc7130
feku1228 <feku1228@gmail.com>
2020-04-29 12:31:55 -0400

    add Replit178.java

---
+++ src/Replit/Replit176.java
@@ -0,0 +1,14 @@
+package Replit;
+
+public class Replit176 {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+In this task, you need to write 2 methods findMax() that will find a maximum number in the array. First method should work with array of integers (int[]) and return int, and second method should work with an array of doubles (double[]) and return double as a result.
+Methods must have the same name and different parameters.
+Fo more information:  https://docs.oracle.com/javase/tutorial/java/javaOO/methods.html
+
+Comment: Methods should be non static and with a return type.
+ */
